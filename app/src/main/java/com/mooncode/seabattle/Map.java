cd2b0e6505dcaafac8a7e1c7da09847890ae344c
Apostol <neketapostolov@gmail.com>
2019-07-11 18:16:31 +0300

    constructor map

--- app/src/main/java/com/mooncode/seabattle/Map.java
+++ app/src/main/java/com/mooncode/seabattle/Map.java
@@ -1,15 +1,16 @@
 package com.mooncode.seabattle;
 
 public class Map {
-    int[][] field = new int[10][10]; // 0 - clear, 1 - ship,
-    void start(){
-        for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
+    private  int[][] field;// = new int[10][10]; // 0 - clear, 1 - ship,
+    public Map(){
+        field = new int[10][10];
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
                 this.field[i][j]=0;
             }
         }
     }
-    int shot(int a,int b){    // 0 - miss, 1 - hurt, 2 - kill
+    int shot(int a, int b){    // 0 - miss, 1 - hurt, 2 - kill
         if (field[a][b] == 0) return 0;
         else return 1;
         //if (field[a][b] == 1) return 1;
